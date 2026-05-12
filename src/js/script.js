cea2435a3ae7fde1e52cc9ef17f5162bd28bbeba
Dmytro <kylon03@gmail.com>
2026-05-12 16:14:12 +0300

    form validation has been added

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 import "/src/sass/style.scss";
+import JustValidate from "just-validate";
 
 const hamburger = document.querySelector(".hamburger"),
   menu = document.querySelector(".menu"),
@@ -18,3 +19,74 @@ const counters = document.querySelectorAll(".skills__progress-procent"),
 counters.forEach((item, i) => {
   lines[i].style.width = item.innerHTML;
 });
+
+try {
+  const validator = new JustValidate("form");
+
+  validator
+    .addField("#name", [
+      {
+        rule: "required",
+        errorMessage: "Please fill the name",
+      },
+      {
+        rule: "minLength",
+        value: 2,
+      },
+    ])
+    .addField("#email", [
+      {
+        rule: "required",
+        errorMessage: "Please fill the email",
+      },
+      {
+        rule: "email",
+      },
+    ])
+    .addField(
+      "#text",
+      [
+        {
+          rule: "required",
+          errorMessage: "Please fill the text field",
+        },
+        {
+          rule: "minLength",
+          value: 5,
+        },
+      ],
+      {
+        errorsContainer: document
+          .querySelector("#text")
+          .parentElement.querySelector(".error-message"),
+      },
+    )
+    .addField(
+      "#checkbox",
+      [
+        {
+          rule: "required",
+          errorMessage: "Please mark the ckeckbox",
+        },
+      ],
+      {
+        errorsContainer: document
+          .querySelector("#checkbox")
+          .parentElement.parentElement.querySelector(".checkbox-error-message"),
+      },
+    )
+    .onSuccess((event) => {
+      const form = event.currentTarget;
+      const formData = new FormData(form);
+
+      fetch("https://httpbin.org/post", {
+        method: "POST",
+        body: formData,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("Success", data);
+          form.reset();
+        });
+    });
+} catch (e) {}
